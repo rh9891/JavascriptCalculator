1d37439a9b6beb4081beb346c4474ea511d5c033
Romie Hecdivert <65568062+rh9891@users.noreply.github.com>
2020-09-17 06:47:23 -0400

    Established constants for the application.

---
+++ script.js
@@ -0,0 +1,9 @@
+// Constants.
+const numberButtons = document.querySelectorAll("[data-number]");
+const operationButtons = document.querySelectorAll("[data-operation]");
+const equalButton = document.querySelector("[data-equals]");
+const deleteButton = document.querySelector("[data-delete]");
+const allClearButton = document.querySelector("[data-allClear]");
+const previousOperandTextElement = document.querySelector("[data-previousOperand]");
+const currentOperandTextElement = document.querySelector("[data-currentOperand]");
+
