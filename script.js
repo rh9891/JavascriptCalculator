f0d031d4463af9226ce19471bbb9492aa2c8d66a
Romie Hecdivert <65568062+rh9891@users.noreply.github.com>
2020-09-17 07:04:57 -0400

    Added the calculator class constructor. Pseudo-coded the functions that will be needed for the calculator to function properly.

--- script.js
+++ script.js
@@ -1,3 +1,35 @@
+// Calculator class constructor that will take all of the inputs for the calculator as well as all of the functions.
+class Calculator {
+    constructor(previousOperandTextElement, currentOperandTextElement) {
+        this.previousOperandTextElement = previousOperandTextElement
+        this.currentOperandTextElement = currentOperandTextElement
+    }
+    // Function to clear out the different variables on the calculator screen.
+    clear() {
+
+    }
+    // Function for removing a single number/symbol on the calculator screen.
+    delete() {
+
+    }
+    // Function to be initiated each time that a user clicks on a number to be added to the calculator screen.
+    appendNumber(number) {
+
+    }
+    // Function for when a user clicks on an operation symbol.
+    chooseOperation(operation) {
+
+    }
+    // Function that takes the value of what is being displayed on the calculator and computes a single value.
+    compute() {
+
+    }
+    // Function that updates the value of what is displayed in the output.
+    updateDisplay() {
+        
+    }
+}
+
 // Constants.
 const numberButtons = document.querySelectorAll("[data-number]");
 const operationButtons = document.querySelectorAll("[data-operation]");
